5743a72dce21d9a5ed994416377b357ea462f7f5
Sahil Jha <120017711+SahilJha1810@users.noreply.github.com>
2026-04-21 12:36:48 +0530

    Add ensemble prediction function for models

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,12 @@
+import torch
+import torch.nn.functional as F
 
+def ensemble_predict(model1, model2, images):
+    out1 = model1(images)
+    out2 = model2(images)
+
+    p1 = F.softmax(out1, dim=1)
+    p2 = F.softmax(out2, dim=1)
+
+    probs = (p1 + p2) / 2
+    return probs
