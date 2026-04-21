f246ba94df520dde045f4f2bd1702f263dd17298
Sahil Jha <120017711+SahilJha1810@users.noreply.github.com>
2026-04-21 12:36:22 +0530

    Add model loading function with timm and torch

--- src/model.py
+++ src/model.py
@@ -1,1 +1,9 @@
+import timm
+import torch
 
+def load_model(model_name, path, device):
+    model = timm.create_model(model_name, pretrained=False, num_classes=2)
+    model.load_state_dict(torch.load(path, map_location=device))
+    model = model.to(device)
+    model.eval()
+    return model
